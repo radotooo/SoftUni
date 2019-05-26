ec93ccee70309d1a06a806409c57fd0b2491c753
Radoto <radotooo@gmail.com>
2019-05-26 18:31:00 +0300

    Initial commit

---
+++ C#Advance/03.Sets and Dictionaries Advanced - Exercise/02.Sets of Elements/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.Sets_of_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int n = num[0];
+            int m = num[1];
+
+            HashSet<int> firstHash = new HashSet<int>();
+            HashSet<int> secondHash = new HashSet<int>();
+
+            fillingHash(firstHash, n);
+            fillingHash(secondHash, m);
+
+            HashSet<int> result = new HashSet<int>(firstHash);
+
+            result.IntersectWith(secondHash);
+            Console.WriteLine(string.Join(" ",result));
+
+
+
+
+        }
+
+        private static void fillingHash(HashSet<int> firstHash, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                firstHash.Add(int.Parse(Console.ReadLine()));
+
+            }
+        }
+    }
+}
---
+++ C#Advance/03.Sets and Dictionaries Advanced - Exercise/05. Count Symbols/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05._Count_Symbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            SortedDictionary<char, int> result = new SortedDictionary<char, int>();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if(!result.ContainsKey(text[i]))
+                {
+                    result[text[i]] = 1;
+                }
+                else
+                {
+                    result[text[i]]++;
+                }
+            }
+
+            foreach (var symbol in result)
+            {
+                Console.WriteLine($"{symbol.Key}: {symbol.Value} time/s");
+            }
+        }
+    }
+}
