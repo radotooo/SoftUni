481334a6c7d11e86734f40cc01ab8fe844281984
Radoto <radotooo@gmail.com>
2019-08-11 21:29:18 +0300

    Exercise

---
+++ C#OOP/02. CSharp-OOP-Basics-Working-with-Abstraction-Exercises-Resources/P03_JediGalaxy/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq;
+
+namespace P03_JediGalaxy
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] dimestions = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int x = dimestions[0];
+            int y = dimestions[1];
+
+            int[,] matrix = new int[x, y];
+
+            int value = 0;
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    matrix[i, j] = value++;
+                }
+            }
+
+            string command = Console.ReadLine();
+            long sum = 0;
+            while (command != "Let the Force be with you")
+            {
+                int[] ivoS = command.Split().Select(int.Parse).ToArray();
+                int[] evil = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                int xEvil = evil[0];
+                int yEvil = evil[1];
+
+                while (xEvil >= 0 && yEvil >= 0)
+                {
+                    if (chekIfIsIn(matrix, xEvil, yEvil))
+                    {
+
+                        matrix[xEvil, yEvil] = 0;
+                    }
+                   
+                    xEvil--;
+                    yEvil--;
+                }
+
+                int xIvan = ivoS[0];
+                int yIvan = ivoS[1];
+
+                while (xIvan >= 0 && yIvan < matrix.GetLength(1))
+                {
+                    if (chekIfIsIn(matrix, xIvan, yIvan))
+                    {
+                        sum += matrix[xIvan, yIvan];
+                    }
+                       
+                    
+
+                    yIvan++;
+                    xIvan--;
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine(sum);
+
+        }
+
+        private static bool chekIfIsIn(int[,] matrix, int xEvil, int yEvil)
+        {
+            return xEvil >= 0 && xEvil < matrix.GetLength(0) && yEvil >= 0 && yEvil < matrix.GetLength(1);
+        }
+            
+    }
+}
---
+++ C#OOP/02. CSharp-OOP-Basics-Working-with-Abstraction-Exercises-Resources/P05_GreedyTimes/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P05_GreedyTimes
+{
+
+    public class Potato
+    {
+        static void Main(string[] args)
+        {
+            long input = long.Parse(Console.ReadLine());
+            string[] safe = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            var bag = new Dictionary<string, Dictionary<string, long>>();
+            long gold = 0;
+            long stones = 0;
+            long moneys = 0;
+
+            for (int i = 0; i < safe.Length; i += 2)
+            {
+                string name = safe[i];
+                long count = long.Parse(safe[i + 1]);
+
+                string command = string.Empty;
+
+                if (name.Length == 3)
+                {
+                    command = "Cash";
+                }
+                else if (name.ToLower().EndsWith("gem"))
+                {
+                    command = "Gem";
+                }
+                else if (name.ToLower() == "gold")
+                {
+                    command = "Gold";
+                }
+
+                if (command == "")
+                {
+                    continue;
+                }
+                else if (input < bag.Values.Select(x => x.Values.Sum()).Sum() + count)
+                {
+                    continue;
+                }
+
+                switch (command)
+                {
+                    case "Gem":
+                        if (!bag.ContainsKey(command))
+                        {
+                            if (bag.ContainsKey("Gold"))
+                            {
+                                if (count > bag["Gold"].Values.Sum())
+                                {
+                                    continue;
+                                }
+                            }
+                            else
+                            {
+                                continue;
+                            }
+                        }
+                        else if (bag[command].Values.Sum() + count > bag["Gold"].Values.Sum())
+                        {
+                            continue;
+                        }
+                        break;
+                    case "Cash":
+                        if (!bag.ContainsKey(command))
+                        {
+                            if (bag.ContainsKey("Gem"))
+                            {
+                                if (count > bag["Gem"].Values.Sum())
+                                {
+                                    continue;
+                                }
+                            }
+                            else
+                            {
+                                continue;
+                            }
+                        }
+                        else if (bag[command].Values.Sum() + count > bag["Gem"].Values.Sum())
+                        {
+                            continue;
+                        }
+                        break;
+                }
+
+                if (!bag.ContainsKey(command))
+                {
+                    bag[command] = new Dictionary<string, long>();
+                }
+
+                if (!bag[command].ContainsKey(name))
+                {
+                    bag[command][name] = 0;
+                }
+
+                bag[command][name] += count;
+                if (command == "Gold")
+                {
+                    gold += count;
+                }
+                else if (command == "Gem")
+                {
+                    stones += count;
+                }
+                else if (command == "Cash")
+                {
+                    moneys += count;
+                }
+            }
+
+            foreach (var x in bag)
+            {
+                Console.WriteLine($"<{x.Key}> ${x.Value.Values.Sum()}");
+                foreach (var item2 in x.Value.OrderByDescending(y => y.Key).ThenBy(y => y.Value))
+                {
+                    Console.WriteLine($"##{item2.Key} - {item2.Value}");
+                }
+            }
+        }
+    }
+}
